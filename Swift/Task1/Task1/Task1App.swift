95d14bf9645dc53028b21c64091b9ce1a2377eab
mkurom <hare.v1v1enne@gmail.com>
2021-06-04 01:19:21 +0900

    first commit

---
+++ Swift/Task1/Task1/Task1App.swift
@@ -0,0 +1,17 @@
+//
+//  Task1App.swift
+//  Task1
+//
+//  Created by 安田誠 on 2021/06/04.
+//
+
+import SwiftUI
+
+@main
+struct Task1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
